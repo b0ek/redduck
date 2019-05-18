61fd2a98f9057c7032c2a848735785a47a3e54d3
B0ek <miguel_boekhold@hotmail.com>
2019-05-19 00:13:16 +0200

    Reformat modules

--- main.py
+++ main.py
@@ -6,36 +6,30 @@ from discord.ext import commands
 import settings
 
 
-initial_extensions = ['modules.image',
-                      'modules.fortnite',
-                      ]
-TOKEN = os.getenv('DISCORD_BOT_API_TOKEN')
-bot = commands.Bot(command_prefix='!')
+initial_extensions = ["modules.image", "modules.fortnite"]
+TOKEN = os.getenv("DISCORD_BOT_API_TOKEN")
+bot = commands.Bot(command_prefix="!")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for extension in initial_extensions:
         try:
             bot.load_extension(extension)
         except Exception as e:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            print(f"Failed to load extension {extension}.", file=sys.stderr)
             traceback.print_exc()
-
-
 
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(bot.user.name)
     print(bot.user.id)
-    print('-----------')
-    
-
+    print("-----------")
 
 
 @bot.command()
 async def greet(ctx):
-    await ctx.send(':wave: Hello there!')
+    await ctx.send(":wave: Hello there!")
 
 
 bot.run(TOKEN)
--- modules/image.py
+++ modules/image.py
@@ -5,32 +5,37 @@ from discord.ext import commands
 import aiohttp
 import os
 
+
 class Images:
+    def __init__(self, bot):
+        self.bot = bot
 
-        def __init__(self, bot):
-                self.bot = bot
-        
-        @commands.command(name='image')
-        async def image(self,ctx, search_arg):
+    @commands.command(name="image")
+    async def image(self, ctx, search_arg):
 
-                '''Searches for a image related to the search arg '''
-                url = 'https://www.googleapis.com/customsearch/v1'
-                params = {
-                        'key': os.getenv('GOOGLE_API_KEY'), 
-                        'cx': os.getenv('GOOGLE_CUSTOM_SEARCH_ENGINE_ID'),
-                        'searchType': 'image', 'q': search_arg, "num": 1, 'safe': 'active'}
-                async with aiohttp.ClientSession() as session:
-                        async with session.get(url, params=params) as resp:
-                                if resp.status == 403:
-                                        return 'Limit exceeded'
-                                data = await resp.json()
-                                image_url = data['items'][0]['link']
-                                # TODO: Check if response is not empty?
-                                embed = discord.Embed(
-                                        title=f'{search_arg}'.capitalize(),
-                                        image=image_url).set_image(url=image_url)
+        """Searches for a image related to the search arg """
+        url = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            "key": os.getenv("GOOGLE_API_KEY"),
+            "cx": os.getenv("GOOGLE_CUSTOM_SEARCH_ENGINE_ID"),
+            "searchType": "image",
+            "q": search_arg,
+            "num": 1,
+            "safe": "active",
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as resp:
+                if resp.status == 403:
+                    return "Limit exceeded"
+                data = await resp.json()
+                image_url = data["items"][0]["link"]
+                # TODO: Check if response is not empty?
+                embed = discord.Embed(
+                    title=f"{search_arg}".capitalize(), image=image_url
+                ).set_image(url=image_url)
 
-                                await ctx.send(embed=embed)
-                                        
+                await ctx.send(embed=embed)
+
+
 def setup(bot):
-        bot.add_cog(Images(bot))
+    bot.add_cog(Images(bot))
